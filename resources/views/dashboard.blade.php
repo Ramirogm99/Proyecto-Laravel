045aa7bb51b94cbaccd43329677cc5c6bd007367
Ramiro López Baltanás <82401950+Ramirogm99@users.noreply.github.com>
2021-11-12 17:50:11 +0100

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,47 +1,89 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <title></title>
-    </head>
-    <body>
-    <div class="responsive-table">             
-                <table class="table table-dark table-hover">
-                 <thead>
-                    <tr>
-                      <th colspan="3" style="text-align: center; font-size:larger;"><u>Ejercicios Tema 2</u></th>
-                    </tr>
-                 </thead>
-                 <tbody style="text-align: center;">
-                    <tr>
-                        <td style="width: 20%">Ejercicio 1</td>
-                        <td style="width: 60%">Creacion de distintos tipos de variables</td>
-                        <td style="width: 20%"><a href="Tema 2\Ejercicio_1.php"><b>Ejercicio_1.php</b></a></td>
-                    </tr>
-                    <tr>
-                        <td style="width: 20%">Ejercicio 2</td>
-                        <td style="width: 60%">Prueba de distintos tipos de comillas</td>
-                        <td style="width: 20%"><a href="Tema 2\Ejercicio_2.php"><b>Ejercicio_2.php</b></a></td>
-                    </tr>
-                    <tr>
-                        <td style="width: 20%">Ejercicio 3</td>
-                        <td style="width: 60%">Prueba de casting de distintos tipos de variables</td>
-                        <td style="width: 20%"><a href="Tema 2\Ejercicio_3.php"><b>Ejercicio_3.php</b></a></td>
-                    </tr>
-                    <tr>
-                        <td style="width: 20%">Ejercicio 4</td>
-                        <td style="width: 60%">Prueba de funciones para Strings</td>
-                        <td style="width: 20%"><a href="Tema 2\Ejercicio_4.php"><b>Ejercicio_4.php</b></a></td>
-                    </tr>
-                    <tr>
-                        <td style="width: 20%">Ejercicio 5</td>
-                        <td style="width: 60%">Distintos usos de array</td>
-                        <td style="width: 20%"><a href="Tema 2\Ejercicio_5.php"><b>Ejercicio_5.php</b></a></td>
-                    </tr>
-                 </tbody>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title></title>
+</head>
+
+<body>
+    <div>
+        <div class="buttons">
+        <a class="btn btn-primary" href="#" role="button" style="background-color: grey;">Registro</a>
+        <a class="btn btn-primary" href="#" role="button">Cerrar sesion</a>
+        </div>
+        <div class="table">
+            <div class="responsive-table" style="margin-left: 5%; margin-right: 5%;">
+                <table class="table table-dark table-hover" >
+                    <thead>
+                        <tr>
+                            <th colspan="6" style="text-align: center; font-size:larger;"><u>Registro</u></th>
+                        </tr>
+                    </thead>
+                    <tbody style="text-align: center;">
+                        <tr>
+                            <td style="width: 20%">Nombre</td>
+                            <td style="width: 20%">apellido</td>
+                            <td style="width: 20%">Hora entrada</td>
+                            <td style="width: 20%">Hora salida</td>
+                            <td style="width: 10%">Centro</td>
+                            <td style="width: 10%">Fecha</td>
+                        </tr>
+                        <tr>
+                            <td style="width: 20%">Nombre</td>
+                            <td style="width: 20%">apellido</td>
+                            <td style="width: 20%">Hora entrada</td>
+                            <td style="width: 20%">Hora salida</td>
+                            <td style="width: 10%">Centro</td>
+                            <td style="width: 10%">Fecha</td>
+                        </tr>
+                        <tr>
+                            <td style="width: 20%">Nombre</td>
+                            <td style="width: 20%">apellido</td>
+                            <td style="width: 20%">Hora entrada</td>
+                            <td style="width: 20%">Hora salida</td>
+                            <td style="width: 10%">Centro</td>
+                            <td style="width: 10%">Fecha</td>
+                        </tr>
+                        <tr>
+                            <td style="width: 20%">Nombre</td>
+                            <td style="width: 20%">apellido</td>
+                            <td style="width: 20%">Hora entrada</td>
+                            <td style="width: 20%">Hora salida</td>
+                            <td style="width: 10%">Centro</td>
+                            <td style="width: 10%">Fecha</td>
+                        </tr>
+                        <tr>
+                            <td style="width: 20%">Nombre</td>
+                            <td style="width: 20%">apellido</td>
+                            <td style="width: 20%">Hora entrada</td>
+                            <td style="width: 20%">Hora salida</td>
+                            <td style="width: 10%">Centro</td>
+                            <td style="width: 10%">Fecha</td>
+                        </tr>
+                        <tr>
+                            <td style="width: 20%">Nombre</td>
+                            <td style="width: 20%">apellido</td>
+                            <td style="width: 20%">Hora entrada</td>
+                            <td style="width: 20%">Hora salida</td>
+                            <td style="width: 10%">Centro</td>
+                            <td style="width: 10%">Fecha</td>
+                        </tr>
+                        <tr>
+                            <td style="width: 20%">Nombre</td>
+                            <td style="width: 20%">apellido</td>
+                            <td style="width: 20%">Hora entrada</td>
+                            <td style="width: 20%">Hora salida</td>
+                            <td style="width: 10%">Centro</td>
+                            <td style="width: 10%">Fecha</td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
-    </body>
+        </div>
+    </div>
+</body>
+
 </html>

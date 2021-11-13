81ac61b1e2e632ff2fc4b99b7e5d902c67e02072
jbranas4 <jbranasmedac@gmail.com>
2021-11-13 15:08:52 +0100

    Index.blade.php y indexstyle.php
    Realizo la vista del index (login) con su correspondiente css

---
+++ resources/views/index.blade.php
@@ -0,0 +1,30 @@
+<html>
+<link href="../css/indexstyle.css" rel="stylesheet" type="text/css">
+<body>
+<div class="contenedor">
+    <header>
+        <div class="cabecera">
+            <h1>Inicio de sesión</h1>
+        </div>
+    </header>
+    <main>
+        <div class="login_box">
+            <div id="box_content">
+                <h2 class="active"> Log In </h2>
+                <form>
+                <input type="text" id="login" class="fadeIn second" name="login" placeholder="Usuario">
+                <input type="text" id="password" class="fadeIn third" name="login" placeholder="Contraseña">
+                <input type="submit" value="Log In">
+                </form>
+            </div>
+        </div>
+    </main>
+
+    <footer>
+        <div class="piedepagina">
+            <h1>footer</h1>
+        </div>
+    </footer>
+</div>
+</body>
+</html>

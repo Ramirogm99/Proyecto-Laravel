331e8e933af9e241651a1ec95ef4ab8b337466fe
Ramiro López Baltanás <82401950+Ramirogm99@users.noreply.github.com>
2021-11-14 19:27:30 +0100

    yep

--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -14,6 +14,7 @@
 </header>
 
 <body>
+    @if(Auth::check())
     <div class="container">
         <div class="row">
             <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 d-flex justify-content-center align-items-center">
@@ -113,7 +114,11 @@
             </div>
         </div>
     </div>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    @else
+    <h1>Por favor inicie sesion</h1>
+    <a class="text-blue-500 hover:text-blue-800" href="{{ route('index')}}">Iniciar sesion</a>
 </body>
 
 <footer>

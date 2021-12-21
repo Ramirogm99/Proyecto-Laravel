1d35460cfca37e603eaabf0e4c69d79566bfd70f
Jose Alberto Zurera <josealbertozurerareyes@gmail.com>
2021-12-21 03:04:08 +0100

    Actualización Proyecto

---
+++ database/migrations/2021_12_10_161928_create_file_in_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFileInTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        
+        Schema::create('file_in', function (Blueprint $table) {
+            $table->increments("id");
+            $table->unsignedInteger("user_id");
+            $table->unsignedInteger("workplace_id");
+            $table->foreign("user_id")->references('id')->on('users')->onDelete("cascade")->onUpdate("cascade");
+            $table->foreign("workplace_id")->references('id')->on('workplaces')->onDelete("cascade")->onUpdate("cascade");
+            $table->datetime("entry_date");
+            $table->datetime("departure_date");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('file_in');
+    }
+}
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,19 +1,22 @@
 <html>
-<link href="../css/indexstyle.css" rel="stylesheet" type="text/css">
+<link href="{{ asset('css/indexstyle.css')}}" rel="stylesheet" type="text/css">
 <body>
 <div class="contenedor">
+
     <header>
         <div class="cabecera">
             <h1>Inicio de sesión</h1>
         </div>
     </header>
+
+    
     <main>
         <div class="login_box">
             <div id="box_content">
                 <h2 class="active"> Log In </h2>
-                <form>
-                <input type="text" id="login" class="fadeIn second" name="login" placeholder="Usuario">
-                <input type="text" id="password" class="fadeIn third" name="login" placeholder="Contraseña">
+                <form action = "/Proyecto-Laravel/public/inicio">
+                <input type="email" id="login" class="fadeIn second" name="login" placeholder="email" required>
+                <input type="password" id="password" class="fadeIn third" name="login" placeholder="password" required>
                 <input type="submit" value="Log In">
                 </form>
             </div>

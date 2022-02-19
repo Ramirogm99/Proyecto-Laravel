6b1c85899c28d69af16614ed886da3fa31b36e6c
Ramiro López Baltanás <82401950+Ramirogm99@users.noreply.github.com>
2022-02-19 12:29:21 +0100

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,7 +5,7 @@
         <div class="login_box">
             <div id="box_content">
                 <h2 class="active"> Log In </h2>
-                <form action = "/Proyecto-Laravel/public/inicio">
+                <form action = "/inicio">
                 <input type="email" id="login" class="fadeIn second" name="login" placeholder="email" required>
                 <input type="password" id="password" class="fadeIn third" name="login" placeholder="password" required>
                 <input type="submit" value="Log In">
